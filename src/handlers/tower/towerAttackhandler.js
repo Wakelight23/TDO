172a9860c8b4c07f1869b10c669f953cfcc7ba27
hyh-test <whdgl3615@naver.com>
2025-01-22 20:27:49 +0900

    [refact] 타워 공격 받는 데이터 검증
    - 타워 공격 payload가 유효한값인지 검증
    - 유저를 찾는데 없는경우 오류 추가

--- src/handlers/tower/towerAttackhandler.js
+++ src/handlers/tower/towerAttackhandler.js
@@ -7,10 +7,23 @@ const towerAttackHandler = async ({ socket, sequence, payload }) => {
   try {
     const { towerId, monsterId } = payload;
 
+    if (typeof towerId !== 'number' || towerId < 100) {
+      throw new Error('towerId는 100 이상의 숫자여야 합니다.');
+    }
+
+    if (typeof monsterId !== 'number' || monsterId < 10000) {
+      throw new Error('monsterId는 10000 이상의 숫자여야 합니다.');
+    }
+
     const user = getUserBySocket(socket);
+    if (!user) {
+      throw new Error('유저를 찾을 수 없습니다.');
+    }
     // 유저에게 저장된 상대 유저의 소켓으로 상대 유저를 찾습니다.
     const enemyUser = getUserBySocket(user.getMatchingUsersocket());
-
+    if (!enemyUser) {
+      throw new Error('상대 유저를 찾을 수 없습니다.');
+    }
     // 상대 유저에게 이 타워가 저 몬스터를 때렸다고 알려줍니다. 그럼 클라에서 때림.
     const enemyTowerAttackNotificationpayload = {
       towerId: towerId,
