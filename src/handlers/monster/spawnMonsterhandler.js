652519498fc3a393035e88bdab9d6ce1b85b0b9a
이이삭 <dlrltn1091@gmail.com>
2025-01-22 10:58:51 +0900

    add.text
    주석 추가.

--- src/handlers/monster/spawnMonsterhandler.js
+++ src/handlers/monster/spawnMonsterhandler.js
@@ -9,16 +9,23 @@ const spawnMonsterHandler = async ({ socket, sequence, payload }) => {
 
     const user = getUserBySocket(socket);
 
+    //유저가 참여하고 있는 게임 세션을 가져옵니다.
     const gameSessions = getJoinGameSessions(user);
 
+    //게임 세션의 몬스터 스폰 카운트를 가져옵니다. 
+    //이 함수는 가져올때마다 세션의 몬스터 카운트가 하나씩 증가해 중복되지 않게 해줍니다.
     const monsterId = gameSessions.getSpawnMonsterCounter();
     
+    //1~5까지 랜덤으로 뽑아줍니다. 몬스터 외형과 스테이터스를 정해줍니다.(정확한 수치는 모름)
     const monsterNumber = Math.floor(Math.random()*(4))+ 1 ;
 
+    //몬스터 객체를 만들어 줍니다. 반드시 이 형태로 만들어야 합니다. 
     const monster = { monsterId , monsterNumber, level:gameSessions.monsterLevel };
 
+    //몬스터를 유저에게 넣어줍니다.
     user.addMonster(monster);
     
+    //그뒤 클라에게 몬스터 스폰을 줍니다.
     const spawnMonsterpayload = {
         monsterId,
         monsterNumber,
--- src/handlers/tower/towerAttackhandler.js
+++ src/handlers/tower/towerAttackhandler.js
@@ -8,8 +8,10 @@ const towerAttackHandler = async ({ socket, sequence, payload }) => {
     const { towerId , monsterId } = payload; 
 
     const user = getUserBySocket(socket);
+    // 유저에게 저장된 상대 유저의 소켓으로 상대 유저를 찾습니다. 
     const enemyUser = getUserBySocket(user.getMatchingUsersocket());
 
+    // 상대 유저에게 이 타워가 저 몬스터를 때렸다고 알려줍니다. 그럼 클라에서 때림.
     const enemyTowerAttackNotificationpayload = {
       towerId: towerId,
       monsterId: monsterId
@@ -17,6 +19,7 @@ const towerAttackHandler = async ({ socket, sequence, payload }) => {
     const packetType = PacketType.ENEMY_TOWER_ATTACK_NOTIFICATION;
     const enemyTowerAttackNotificationResponse = createResponse(packetType, enemyTowerAttackNotificationpayload, sequence);
     enemyUser.socket.write(enemyTowerAttackNotificationResponse);
+    // ^상대 유저에게 보냅니다. 모르겠으면 안보내 보면 됨. 
 
   } catch (error) {
     console.error(error);
